8a28384709bb9847dded331365bc4b46c34a11f4
730530507 <730530507@qq.com>
2016-09-28 18:22:30 +0800

    增加ORM

---
+++ MyFramework/Test/Test.Console.Data/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Entities;
+using Data.Repositories;
+using MySqlSugar;
+
+namespace Test.Console.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                T_exclude_websitesRepository repository = new T_exclude_websitesRepository();
+                repository.Insert(new T_exclude_websites()
+                {
+                    WebSite_Name="asddd",
+                    WebSite_Url="dsasdfgf",
+                    create_time=DateTime.Now,
+                    is_erased=0
+                });
+                var model= repository.Queryable().Where(p => p.ID==1).FirstOrDefault();
+            }
+            catch (Exception exception)
+            {
+                System.Console.WriteLine(exception.Message);
+            }
+            System.Console.Read();
+        }
+    }
+}
