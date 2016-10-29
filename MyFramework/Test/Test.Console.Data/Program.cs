34c95414d263fc46567c5ab0aebc98be94102322
730530507 <730530507@qq.com>
2016-10-29 23:14:43 +0800

    63

--- MyFramework/Test/Test.Console.Data/Program.cs
+++ MyFramework/Test/Test.Console.Data/Program.cs
@@ -20,15 +20,7 @@ namespace Test.Console.Data
                 var builder = new ContainerBuilder();
                 builder.RegisterType<Services.Demo.Test>();
                 builder.RegisterType<T_exclude_websitesRepository>().As<IT_exclude_websitesRepository>();
-                using (var container = builder.Build())
-                {
-                    WebSite_Name="asddd",
-                    WebSite_Url="dsasdfgf",
-                    create_time=DateTime.Now,
-                    is_erased=0
-                });
-                var model= repository.Queryable().Where(p => p.ID==4).FirstOrDefault();
-                System.Console.WriteLine(model.WebSite_Name);
+               
             }
             catch (Exception exception)
             {
--- MyFramework/Web/AdminCenter/App_Start/RouteConfig.cs
+++ MyFramework/Web/AdminCenter/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AdminCenter
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "AdminLte", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
